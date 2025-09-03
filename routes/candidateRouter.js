82657e0a071eb3b761576aa077ac1c0c850e60ba
N Jayant Rao <njayantrao@gmail.com>
2025-09-03 07:18:58 +0530

    added gitignore file

--- routes/candidateRouter.js
+++ routes/candidateRouter.js
@@ -4,6 +4,7 @@ const candidateRouter= router
 import { Candidate } from "./../models/candidate.js"
 import { User } from "./../models/users.js"
 import {jwtAuthMiddleware,generateToken} from "./../jwt.js";
+//import passport from "passport";
 
 const checkAdmin= async (userID)=>{
   try{
@@ -99,6 +100,77 @@ router.delete("/:candidateID",jwtAuthMiddleware,async (req,res)=>{
         console.log("The error is ",error)
         res.send("internal server error").status(500)
     }
+})
+
+//Voting phase begins
+
+router.post("/vote/:candidateID",jwtAuthMiddleware,async (req,res)=>{
+
+  const candidateId= req.params.candidateID
+  const userId= req.user.id
+
+  try {
+    //Find Candidate through candidateId
+
+    const candidate= await Candidate.findById(candidateId)
+    if(!candidate)
+    {
+      return res.status(404).send("Candidate not found")
+    }
+
+    //Find user through userId
+
+    const user= await User.findById(userId)
+    if(!user)
+    {
+      return res.status(404).send("User not found")
+    }
+
+    //If user has already voted
+    if(user.isVoted){
+      return res.status(400).send("You have already voted")
+    }
+
+    //For admin restriction
+    if(user.role === "Admin"){
+      return res.status(403).send("Admin Can't Vote")
+    }
+
+    //Update User info. in Candidate schema
+    candidate.votes.push({user:userId})
+    candidate.voteCount++;
+    await candidate.save();
+
+    //Update User info.
+    user.isVoted= true;
+    await user.save();
+
+    res.status(200).send("Voted successfully")
+
+  } catch (error) {
+      console.log("The error is ",error)
+      res.send("internal server error").status(500)
+  }
+})
+
+//Vote counting
+
+router.get("/vote/count", async (req,res)=>{
+  try {
+    //Get candidate info. in sorted order
+    const candidate= await Candidate.find().sort({voteCount:"desc"})
+
+    //candidate should only return user name and Votecount
+    const voteRecord= candidate.map((data=>{
+      return{ party: data.party,
+             count: data.voteCount
+      }
+    }))
+    res.status(200).json({voteRecord})
+  } catch (error) {
+    console.log("The error is ",error)
+    res.send("internal server error").status(500)
+  }
 })
 
 
